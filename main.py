4158b8db9635dd18baad4b6ec5ed992f2b562b2d
Andresito126 <creed126ax@gmail.com>
2025-12-03 23:04:22 -0600

    Add: tkinter added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import re
+import tkinter as tk
+from tkinter import filedialog, messagebox, scrolledtext
 
 # Expresiones regulares
 ER_PALABRA_BASICA=r"^[a-zñáéíóúü]+$"
@@ -61,8 +63,77 @@ def analizar_texto(palabras_validas, texto_entrada):
             
     return formato
 
+#TKINTER
+def interfaz():
+    print("interfaz papu")
+    RUTA_CSV = r"/Users/Andre/Documentos/1TareasUp/7moCuatri/Automatas/analizador-lexico-para-palabras-validas-en-espanol/palabras_espanol.csv"
+    palabras_validas = leer_CSV(RUTA_CSV, ",")
+
+# donde se hace la ventanaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa 
+    root = tk.Tk()
+    root.title("Analizador Léxico ")
+    root.geometry("720x550")
+    root.resizable(False, False)
+    print("TOKEN".ljust(25), "LEXEMA")
+    print("-" * 50)
+
+    tk.Label(root, text="Ingrese el texto a analizar:").pack(pady=5)
+
+    entrada_texto = scrolledtext.ScrolledText(root, width=80, height=8)
+    entrada_texto.pack()
+
+    tk.Label(root, text="Resultado de tokens (Token - Lexema):").pack(pady=5)
+    salida_texto = scrolledtext.ScrolledText(root, width=80, height=15)
+    salida_texto.pack()
+
+    def convertir_a_tabla(diccionario_tokens, texto_original):
+        tabla = []
+
+        tokens_ordenados = tokenizar(texto_original)
+
+        for palabra in tokens_ordenados:
+            if palabra in diccionario_tokens["PALABRA_VALIDA_ESPANOL"]:
+                tabla.append(("PALABRA_VALIDA_ESPANOL", palabra))
+            elif palabra in diccionario_tokens["PUNTUACION"]:
+                tabla.append(("PUNTUACION", palabra))
+            elif palabra in diccionario_tokens["DIGITO"]:
+                tabla.append(("DIGITO", palabra))
+            else:
+                tabla.append(("ERROR_ORTOGRAFICO", palabra))
+
+        return tabla
+
+
+#boton 
+    def ejecutar_analisis():
+        texto = entrada_texto.get("1.0", tk.END).strip()
+
+        if texto == "":
+            messagebox.showwarning("Escriba un texto para analizar.")
+            return
+
+        resultado_diccionario = analizar_texto(palabras_validas, texto)
+
+# limpiar 
+        salida_texto.delete("1.0", tk.END)
+
+# encabezado
+        salida_texto.insert(tk.END, "TOKEN".ljust(28) + "LEXEMA\n")
+        salida_texto.insert(tk.END, "-" * 50 + "\n")
+
+        tabla = convertir_a_tabla(resultado_diccionario, texto)
+
+        for token, lexema in tabla:
+            salida_texto.insert(tk.END, f"{token.ljust(28)}{lexema}\n")
+
+
+
+    tk.Button(root, text="Analizar texto", command=ejecutar_analisis, width=20).pack(pady=10)
+
+    root.mainloop()
+
 def main():
-    palabras_validas = leer_CSV(r'/Users/kev29.06/Documents/Kevin/C7/Lenguajes y automátas/Project-1/palabras_espanol.csv', ',') 
+    palabras_validas = leer_CSV(r'/Users/Andre/Documentos/1TareasUp/7moCuatri/Automatas/analizador-lexico-para-palabras-validas-en-espanol/palabras_espanol.csv', ',') 
 
     tokens = analizar_texto(palabras_validas=palabras_validas, texto_entrada='El perro corrio 3 veses acia la kasa. ¿tenemos computadoras?')
 
@@ -70,4 +141,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+    interfaz()
+
 
